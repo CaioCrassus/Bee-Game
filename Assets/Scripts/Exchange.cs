0bae327f3153b1e1219129aaecd3bffdd2e30511
Sergio Filho <sergiofffilho@gmail.com>
2019-01-26 08:40:07 +0000

    Panels de acordo com a escolha de compra

--- Assets/Scripts/Exchange.cs
+++ Assets/Scripts/Exchange.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Exchange : MonoBehaviour
 {
@@ -16,9 +17,26 @@ public class Exchange : MonoBehaviour
         
     }
 
-    public void openPanelExchange() 
-    {
+    //if bee false: bee = worker else: bee = soldier
+    public void openPanelExchange(bool bee){
         gameObject.SetActive(true);
+        if (bee){
+            gameObject.GetComponent<Image>().color = new Color(1, 0.92f, 0.016f, 0.5f);
+            gameObject.transform.Find("iBeeWorker").gameObject.SetActive(true);
+            gameObject.transform.Find("iBeeSoldier").gameObject.SetActive(false);
+            gameObject.transform.Find("btnPurchase").Find("iPurchaseWorker").gameObject.SetActive(true);
+            gameObject.transform.Find("btnPurchase").Find("iPurchaseSoldier").gameObject.SetActive(false);
+
+        }
+        else {
+            gameObject.GetComponent<Image>().color = new Color(1, 0, 0, 0.5f);
+            gameObject.transform.Find("iBeeWorker").gameObject.SetActive(false);
+            gameObject.transform.Find("iBeeSoldier").gameObject.SetActive(true);
+            gameObject.transform.Find("btnPurchase").Find("iPurchaseWorker").gameObject.SetActive(false);
+            gameObject.transform.Find("btnPurchase").Find("iPurchaseSoldier").gameObject.SetActive(true);
+
+        }
+        
     }
 
     public void ClosePanelExchange() {
