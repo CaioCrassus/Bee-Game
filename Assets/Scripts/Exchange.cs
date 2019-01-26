38fc779e54d430b51192dff802a4084ef723fd6a
Sergio Filho <sergiofffilho@gmail.com>
2019-01-26 08:00:13 +0000

    Panel para exchange abrindo e fechando

---
+++ Assets/Scripts/Exchange.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exchange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void openPanelExchange() 
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void ClosePanelExchange() {
+        gameObject.SetActive(false);
+    }
+}
