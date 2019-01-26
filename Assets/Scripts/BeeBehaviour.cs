7ee9e7fd44ff1913210b22ab3b77802de642dd15
Janderson Araujo Brito <j.a.brito@live.com>
2019-01-26 02:34:53 +0000

    Adicionando cena da bee voando

---
+++ Assets/Scripts/BeeBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeeBehaviour : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float moveDirection;
+    private bool moving = false;
+    private float speed = 100f;
+
+    public float jumpForce;
+    public float atackForce;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        moveDirection = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        rb.AddForce(transform.forward * moveDirection);
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            // the cube is going to move upwards in 10 units per second
+            rb.velocity = new Vector3(0, jumpForce, moveDirection);
+            moving = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // the cube is going to move upwards in 10 units per second
+            rb.velocity = new Vector3(0, atackForce, moveDirection);
+            moving = true;
+        }
+    }
+}
